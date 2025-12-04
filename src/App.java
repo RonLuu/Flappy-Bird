bc7dd4330e4fd515159af4aa130e54b8b29c42d1
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-12-04 16:32:46 +0700

    Add gitignore

--- src/App.java
+++ src/App.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // int width = 360;
-        // int height = 640;
 
         JFrame jFrame = new JFrame("My Flappy Bird");
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
